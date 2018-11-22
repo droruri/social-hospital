ecceee508b9a9891e7216f007954a622a90cedef
droruri <droruri@gmail.com>
2018-11-22 22:05:09 +0200

    upgrading the component communities-selection

---
+++ src/app/core/models/community.ts
@@ -0,0 +1,4 @@
+export interface Community {
+  name: string;
+  isUserInCommunity: boolean;
+}
--- src/app/home/pages/communities-selection/communities-selection.component.ts
+++ src/app/home/pages/communities-selection/communities-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Community} from '../../../core/models/community';
 
 @Component({
   selector: 'communities-selection',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./communities-selection.component.scss']
 })
 export class CommunitiesSelectionComponent implements OnInit {
+
+  communityDescription:string;
+
+  communities:Community[]=
+    [
+      {name:'מחלקת דם', isUserInCommunity: false},
+      {name:'מחלקת בעיות לב', isUserInCommunity: false},
+      {name:'מחלקת סרטן', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+      {name:'מחלקת אסתמה', isUserInCommunity: false},
+    ];
 
   constructor() { }
 
